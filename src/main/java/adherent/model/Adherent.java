28e464254bc8895f8de59c3f24054dc27ac2fd45
jonathan42 <jonathan.fey@laposte.net>
2017-02-27 14:21:54 +0100

    maj adherent

---
+++ src/main/java/adherent/model/Adherent.java
@@ -0,0 +1,54 @@
+package adherent.model;
+
+import javax.persistence.*;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import emprunt.model.Emprunt;
+
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Table (name="adherent")
+public class Adherent {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@NotBlank
+	private String nom;
+	
+	@NotBlank
+	private String prenom;
+	
+	@NotBlank
+	private String dateNaissance;
+	
+	@NotBlank
+	private String adresse;
+	
+	@NotBlank
+	private Integer codePostal;
+	
+	@NotBlank
+	private String ville;
+	
+	@NotBlank
+	private String email;
+	
+	@OneToOne
+	private Cotisation cotisation;
+	
+	@OneToMany (mappedBy = "abonne")
+	private List<Emprunt> emprunts;
+
+	
+	public Adherent(){
+	}
+	
+	
+	
+}
