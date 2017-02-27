509e7e63027a1534014bea920c8c61c8c8f8580d
FabienPinard <pinard.fabien@gmail.com>
2017-02-27 14:42:10 +0100

    avance dans adherent

--- src/main/java/adherent/model/Adherent.java
+++ src/main/java/adherent/model/Adherent.java
@@ -25,7 +25,8 @@ public class Adherent {
 	private String prenom;
 	
 	@NotBlank
-	private String dateNaissance;
+	@Temporal(TemporalType.DATE)
+	private Date dateNaissance;
 	
 	@NotBlank
 	private String adresse;
@@ -42,12 +43,113 @@ public class Adherent {
 	@OneToOne
 	private Cotisation cotisation;
 	
-	@OneToMany (mappedBy = "abonne")
+	@OneToMany (mappedBy = "adherent")
 	private List<Emprunt> emprunts;
 
 	
 	public Adherent(){
 	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+
+
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+
+	public Integer getCodePostal() {
+		return codePostal;
+	}
+
+
+	public void setCodePostal(Integer codePostal) {
+		this.codePostal = codePostal;
+	}
+
+
+	public String getVille() {
+		return ville;
+	}
+
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public Cotisation getCotisation() {
+		return cotisation;
+	}
+
+
+	public void setCotisation(Cotisation cotisation) {
+		this.cotisation = cotisation;
+	}
+
+
+	public List<Emprunt> getEmprunts() {
+		return emprunts;
+	}
+
+
+	public void setEmprunts(List<Emprunt> emprunts) {
+		this.emprunts = emprunts;
+	}
+	
 	
 	
 	
