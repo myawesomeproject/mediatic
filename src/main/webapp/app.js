3442c4e36c4b999631869e6ac22db26d3a6e918c
Fidan <kevin.fidan@gmail.com>
2017-03-14 12:17:38 +0100

    Routes

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -10,6 +10,30 @@ angular.module('MyApp',['ngRoute','MyApp.Connexion'])
 			title: 'Connexion'
 		});	
 		
+		$routeProvider.when('/adherentvisualisation', {
+			controller :'adherentCtrl',
+			templateUrl : './template/adherent.html',
+			title: 'Adhérent'
+		});	
+		
+		$routeProvider.when('/adherentrecherche', {
+			controller :'adherentrechercheCtrl',
+			templateUrl : './template/adherentrecherche.html',
+			title: 'Liste des adhérents'
+		});	
+		
+		$routeProvider.when('/mediavisualisation', {
+			controller :'mediaCtrl',
+			templateUrl : './template/media.html',
+			title: 'Média'
+		});	
+		
+		$routeProvider.when('/mediatrecherche', {
+			controller :'mediarechercheCtrl',
+			templateUrl : './template/mediarecherche.html',
+			title: 'Liste des médias'
+		});	
+		
 	/*	$routeProvider.when('/book/:id', {
 			controller :'bookCtrl',
 			templateUrl : './view-book/book.html',
