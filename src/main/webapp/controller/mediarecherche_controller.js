e88ed0b189d7fe88da1240322baadbe6bf1fe350
Fidan <kevin.fidan@gmail.com>
2017-03-15 15:52:26 +0100

    mediarecherche avec filtres presques terminés

--- src/main/webapp/controller/mediarecherche_controller.js
+++ src/main/webapp/controller/mediarecherche_controller.js
@@ -15,14 +15,21 @@
 			$scope.listeMedia = undefined;
 
 			ListeMediaService.getList().then(function(data) {
-				
+				var estlibre = true;
 				$scope.listeMedia = data;
-				
+				for (var index in $scope.listeMedia){
+					var media = $scope.listeMedia[index];
+					console.log(media);
+					if (media.emprunteur!==undefined){
+						media.etat = 'PRETE';
+					} else {
+						media.etat = 'LIBRE';
+					}
+				}				
 			});		
 	    
-		    $scope.lancerRechercheAvancee = function(){
-		    	console.log($scope.modelRechercheAvancee);
-		    	$scope.filterRechercheAvancee = $scope.modelRechercheAvancee;
+		    $scope.lancerRechercheAvancee = function(){		    	
+		    	$scope.filterRechercheAvancee = angular.copy($scope.modelRechercheAvancee);
 		    }
 		 
 		});
