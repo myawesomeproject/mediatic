a2a39a26c2b8df162f02de8db5ee6666174ad60c
jonathan42 <jonathan.fey@laposte.net>
2017-03-15 12:16:37 +0100

    creation de l'ajout d'adherent

--- src/main/webapp/controller/adherentcreation_controller.js
+++ src/main/webapp/controller/adherentcreation_controller.js
@@ -4,11 +4,57 @@
 
 	angular
 		.module('MyApp.Adherent')
-		.controller('AdherentCreationCtrl',function($rootScope) {
+		.controller('AdherentCreationCtrl',function($rootScope,AdherentService,$scope,dateFilter) {
 		
 			$rootScope.title = "Création d'adhérents" ;
 			
+			/*var datatest = {
+					"nom": "toto",
+				    "prenom": "toto",
+				    "date_naissance": "1992-02-28",
+				    "email": "maitre@troy.com",
+				    "adresse": {
+				      "ligne1": "3eme maison",
+				      "ligne2": "chambre du père",
+				      "codepostal": "3-333",
+				      "ville": "Eckmul"
+				    },
+				    "cotisation": {
+				      "debut": "2017-01-01",
+				      "fin": "2018-01-01",
+				      "montant": 20
+				    },
+				    "age": 23,
+				    "emprunt": [],
+				    "nombre_media": 0
+				};*/
+			$scope.data ={};
 			
+			
+			
+			
+			
+			$scope.ajoute = function(data) {
+				
+				data.adresse.ligne2 = " ";
+				data.emprunt = [];
+				data.nombre_media = 0;
+				data.age = 25; // faire une function age;
+				data.date_naissance = dateFilter(data.date_naissance, 'yyyy-MM-dd');
+				data.cotisation.debut = dateFilter(data.cotisation.debut, 'yyyy-MM-dd');
+				data.cotisation.fin = dateFilter(data.cotisation.debut, 'yyyy-MM-dd'); // function ajouté 1 ans
+				
+				console.log(data);
+				
+		        return AdherentService.add(data);
+		    };
+		    
+		    $scope.remove = function(){
+		    	$scope.data ={};
+		    }
+						
+						
+				
 		});
 
 })();
---
+++ src/main/webapp/service/adherent_service.js
@@ -0,0 +1,33 @@
+(function(){
+	'use strict';
+	
+	angular
+		.module('MyApp.Adherent')
+	
+		.service('AdherentService', function($rootScope,UrlService,$http){
+			var panier = [];
+			//var seft = this;
+			this.add = function(membre){
+				var url = UrlService.getAdherentCreationUrl();
+				
+				$http.post(url, membre).then(function(reponse) {
+					$rootScope.datas = reponse.data;
+				
+				});	
+				
+			};
+			this.getHT = function(ligne){
+				return ligne.article.price *ligne.qte;
+				
+			}
+
+			$rootScope.getPanierServiceSize = function(){
+				return panier.reduce(function(previous, item){
+					return previous +item.qte;
+				}, 0);
+			}
+			return this;
+		})
+
+})();
+
