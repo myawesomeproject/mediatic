6373b7f3b58408b9f1cf40effa7daca7e3b73ffc
Fidan <kevin.fidan@gmail.com>
2017-03-15 17:07:35 +0100

    Push avant changement PC

--- src/main/webapp/controller/mediarecherche_controller.js
+++ src/main/webapp/controller/mediarecherche_controller.js
@@ -6,20 +6,19 @@
 		.module('MyApp.Media')
 		.controller('MediaRechercheCtrl',function($scope, $rootScope, ListeMediaService, MediaRechercheService) {
 		
-			$rootScope.title = "Liste des médias" ;
-			
-			$scope.recherche = MediaRechercheService;
-			
-			$scope.modeRechercheAvancee=false; //pour cacher la barre de recherche avancée à l'initialisation
-			
+			$rootScope.title = "Liste des médias" ;			
+			$scope.recherche = MediaRechercheService; //inutile atm			
+			$scope.modeRechercheAvancee=false; //pour cacher la barre de recherche avancée à l'initialisation			
 			$scope.listeMedia = undefined;
-
+			
+			$scope.tdTri = 'titre'; // Type de tri par défault
+			$scope.triInverse = false;  // Sens de tri par défault
+			
 			ListeMediaService.getList().then(function(data) {
 				var estlibre = true;
 				$scope.listeMedia = data;
 				for (var index in $scope.listeMedia){
-					var media = $scope.listeMedia[index];
-					console.log(media);
+					var media = $scope.listeMedia[index];					
 					if (media.emprunteur!==undefined){
 						media.etat = 'PRETE';
 					} else {
