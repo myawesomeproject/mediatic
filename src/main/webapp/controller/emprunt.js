9a6ecfab1ea668db0a3231b3b07522e74b44e10b
FabienPinard <pinard.fabien@gmail.com>
2017-03-16 17:20:53 +0100

    Creation de l'emprunt côté media

--- src/main/webapp/controller/emprunt.js
+++ src/main/webapp/controller/emprunt.js
@@ -4,11 +4,13 @@
 
 	angular
 		.module('MyApp.Emprunt',['ngRoute','MyApp.Adherent','MyApp.Media'])
-		.controller('EmpruntCtrl',function($rootScope,$scope,UrlService,AdherentService,MediaService,$http,$routeParams) {
+		.controller('EmpruntCtrl',function($rootScope,$scope,UrlService,AdherentService,dateFilter,MediaService,$http,$routeParams) {
 					
 			var url = UrlService.getMediaAccessionUrl();
+			var url2 = UrlService.getEmpruntAjoutUrl();
 			
-			var media=[];
+			var emprunt={};
+			var media={};
 			
 			$http.get(url,{params:{"id": $routeParams.id}}).then(function(reponse) {
 				
@@ -17,14 +19,11 @@
 			});
 			
 			
-			$scope.emprunt = function(adherent) {
-				
-				media.emprunteurs[media.emprunteurs.length]=adherent;
-				media.retour= new Date() + 1;
-				
-
-				
-		        return MediaService.update(media);
+			$scope.addemprunt = function(adherent) {
+				emprunt = {id_adherent : adherent.id,id_media : media.id, depart : dateFilter( new Date(),'yyyy-MM-dd')};
+				$http.post(url2,emprunt);
 		    };
+		    
+		    
 		});
 })();
