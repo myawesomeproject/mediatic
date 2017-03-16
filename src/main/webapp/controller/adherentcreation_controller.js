e8b2d17cd4f76044f4f4ae39a3338266a8547ee4
jonathan42 <jonathan.fey@laposte.net>
2017-03-16 15:36:56 +0100

    commit creation maj de ajout adherent ( fiunction age et abo + 1ans)

--- src/main/webapp/controller/adherentcreation_controller.js
+++ src/main/webapp/controller/adherentcreation_controller.js
@@ -4,7 +4,7 @@
 
 	angular
 		.module('MyApp.Adherent')
-		.controller('AdherentCreationCtrl',function($rootScope,AdherentService,$scope,dateFilter) {
+		.controller('AdherentCreationCtrl',function($rootScope,AdherentService,$scope,dateFilter,$location) {
 		
 			$rootScope.title = "Création adhérents" ;
 			
@@ -28,7 +28,7 @@
 				    "emprunt": [],
 				    "nombre_media": 0
 				};*/
-			$scope.data ={};
+			$scope.data ={	};
 			
 			
 			
@@ -39,22 +39,66 @@
 				data.adresse.ligne2 = " ";
 				data.emprunt = [];
 				data.nombre_media = 0;
-				data.age = 25; // faire une function age;
 				data.date_naissance = dateFilter(data.date_naissance, 'yyyy-MM-dd');
-				data.cotisation.debut = dateFilter(data.cotisation.debut, 'yyyy-MM-dd');
-				data.cotisation.fin = dateFilter(data.cotisation.debut, 'yyyy-MM-dd'); // function ajouté 1 ans
 				
-				console.log(data);
+				data.age = calculAge(data.date_naissance); // faire une function age;
+
 				
-		        return AdherentService.add(data);
+				if (data.cotisation.debut != undefined){
+					
+					data.cotisation.debut = dateFilter(data.cotisation.debut, 'yyyy-MM-dd');
+					data.cotisation.fin = Abonnement1Y(data.cotisation.debut); // function ajouté 1 ans
+				}
+				else {
+					data.cotisation.debut = " ";
+					data.cotisation.fin = " ";
+				}
+					
+				//console.log(data);
+				
+				
+		        AdherentService.add(data);
+		        $location.path('/adherentrecherche');
+		        
 		    };
 		    
 		    $scope.remove = function(){
 		    	$scope.data ={};
 		    }
 						
-						
-				
+
+			function calculAge(strDate) {
+			    strDate = strDate.split('-');
+			    var birthMonth = strDate[1]-1, // (les mois commencent à 0)
+			        birthDay = strDate[2], 
+			        now = new Date(),
+			        nowMonth = now.getMonth(),
+			        nowDay = now.getDate(),
+			        age = now.getFullYear()-strDate[0];
+			     
+			   // console.log('mois =', birthMonth ,'jours =', birthDay,'mois =', nowMonth, 'jours =',nowDay, age);
+			    // Si la date d'anniversaire n'est pas encore passée, on corrige l'age
+			   // if(nowMonth<birthMonth || nowMonth==birthMonth && nowDay<birthDay) {
+			  //      age--;
+			  //  }
+			    return age;
+			}
+			
+			function Abonnement1Y(strDate) {
+			    strDate = strDate.split('-');
+			    var birthMonth = strDate[1], 
+			        birthDay = strDate[2], 
+			        age = strDate[0];
+			     
+			    age = parseInt(age) + 1;
+			    var str = age+"-"+birthMonth+"-"+birthDay;
+			    
+			    return str;
+			}
+			 
+			//console.log(Abonnement1Y('1994-02-01')); // 18
+			//console.log(calculAge('1994-05-05')); // 17 
+			
 		});
 
 })();
