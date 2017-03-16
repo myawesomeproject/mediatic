018d60c56482fea78145aa0caf38fbd34f552b4f
FabienPinard <pinard.fabien@gmail.com>
2017-03-16 11:57:49 +0100

    Mise en place de l'ajout de média

--- src/main/webapp/controller/mediacreation_controller.js
+++ src/main/webapp/controller/mediacreation_controller.js
@@ -4,11 +4,20 @@
 
 	angular
 		.module('MyApp.Media')
-		.controller('MediaCreationCtrl',function($rootScope) {
+		.controller('MediaCreationCtrl',function($rootScope,$scope,MediaService) {
 		
 			$rootScope.title = "Création média" ;
 			
+			$scope.data ={};
 			
+			$scope.ajoute = function(data) {
+				console.log(data);
+				MediaService.add(data);
+				};
+				
+			$scope.remove = function(){
+			    $scope.data ={};
+			}
 		});
 
 })();
