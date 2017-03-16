9c1d27b8c35b9f5f5e8e536f65407072fd8efac9
FabienPinard <pinard.fabien@gmail.com>
2017-03-16 15:38:06 +0100

    Fix de visualisation et ajout media

--- src/main/webapp/controller/emprunt.js
+++ src/main/webapp/controller/emprunt.js
@@ -3,7 +3,28 @@
 	'use strict';
 
 	angular
-		.module('MyApp.Emprunt',['ngRoute'])
+		.module('MyApp.Emprunt',['ngRoute','MyApp.Adherent','MyApp.Media'])
+		.controller('EmpruntCtrl',function($rootScope,$scope,UrlService,AdherentService,MediaService,$http,$routeParams) {
+					
+			var url = UrlService.getMediaAccessionUrl();
+			
+			var media=[];
+			
+			$http.get(url,{params:{"id": $routeParams.id}}).then(function(reponse) {
+				
+				media = reponse.data;
+				$scope.media = reponse.data;
+			});
+			
+			
+			$scope.emprunt = function(adherent) {
+				
+				media.emprunteurs[media.emprunteurs.length]=adherent;
+				media.retour= new Date() + 1;
+				
 
-
+				
+		        return MediaService.update(media);
+		    };
+		});
 })();
