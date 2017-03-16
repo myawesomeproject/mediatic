62444999283d447abe50f7e4748663080396ff68
kfidan <kevin.fidan@gmail.com>
2017-03-16 15:13:58 +0100

    Mediarecherche complètement operationnel

--- src/main/webapp/service/listeMediaService.js
+++ src/main/webapp/service/listeMediaService.js
@@ -8,7 +8,7 @@
 
 						//var url = 'https://api.mongolab.com/api/1/databases/books/collections/books/?apiKey=d3qvB8ldYFW2KSynHRediqLuBLP8JA8i'
 						var url = UrlService.getMediaRechercheUrl();
-						// console.log(url); pour faire apparaitre l'url de la liste des médias
+						// console.log(url); //pour faire apparaitre l'url de la liste des médias
 						var promise = $http.get(url).then(function(response) {							
 							return response.data
 						}, function(){
