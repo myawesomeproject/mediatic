e9c7de053bd9ccb631386dc131ab510138ba729c
jonathan42 <jonathan.fey@laposte.net>
2017-03-31 11:11:34 +0200

    conversion en JPA hibernate spring

--- src/main/java/fr/adherent/dao/AdherentDAO.java
+++ src/main/java/fr/adherent/dao/AdherentDAO.java
@@ -1,20 +1,18 @@
 
 package fr.adherent.dao;
 
-import fr.adherent.model.*;
-import fr.emprunt.model.Emprunt;
-import fr.media.model.*;
-import fr.service.DatabaseHelper;
-import fr.service.GenericDAO;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
 
 import org.hibernate.Hibernate;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import java.util.List;
+import fr.adherent.model.Adherent;
+import fr.emprunt.model.Emprunt;
+import fr.service.GenericDAO;
 
 @Service
 @Repository
@@ -35,25 +33,22 @@ public class AdherentDAO extends GenericDAO<Adherent> {
 	}
 
 	public List<Emprunt> MediasBoughtBy2Requests(Long AdherentId) {
-		EntityManager entityManager = DatabaseHelper.createEntityManager();
-		List<Emprunt> empruntMedia = entityManager.find(Adherent.class, AdherentId).getEmprunts();
+		List<Emprunt> empruntMedia = em.find(Adherent.class, AdherentId).getEmprunts();
 		Hibernate.initialize(empruntMedia);
 		return empruntMedia;
 	}
 
 	public Adherent MediasEmprunt(Long AdherentId) {
-		EntityManager entityManager = DatabaseHelper.createEntityManager();
 		String qlQuery = "SELECT a " + "FROM Adherent a " + "LEFT JOIN FETCH a.emprunts e " + "LEFT JOIN FETCH e.media "
 				+ "WHERE a.id=:id";
-		TypedQuery<Adherent> query = entityManager.createQuery(qlQuery, Adherent.class);
+		TypedQuery<Adherent> query = em.createQuery(qlQuery, Adherent.class);
 		query.setParameter("id", AdherentId);
 		return query.getSingleResult();
 	}
 	
 
 	public List<Adherent> getListAdherent() {
-		EntityManager entityManager = DatabaseHelper.createEntityManager();
-		TypedQuery<Adherent> query = entityManager.createQuery("from Adherent", Adherent.class);
+		TypedQuery<Adherent> query = em.createQuery("from Adherent", Adherent.class);
 		List<Adherent> adherents = query.getResultList();
 		return adherents;
 	}
