2006176965a92ad9e67aace5c4b7695b9466cada
shady <shady@PC-Jonathan>
2017-04-01 18:35:55 +0200

    up adherent pour la creation v1 non fonctionnel encore

--- src/main/java/fr/controller/AdherentController.java
+++ src/main/java/fr/controller/AdherentController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,15 +19,17 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.dao.AdherentDAO;
 import fr.model.Adherent;
 
-@RestController
+@Controller
 @RequestMapping("/resource")
+@Transactional
+
 public class AdherentController {
 
 	@Autowired
 	private AdherentDAO adherentDAO;
 	
 	@ResponseBody
-	@RequestMapping(value = "/adherent.recherche", method = RequestMethod.GET)
+	@RequestMapping(value = "/adherent.recherche", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Adherent> display() {
 		List<Adherent> result = adherentDAO.getListAdherent();
 		return result;
@@ -36,12 +42,20 @@ public class AdherentController {
 //	}
 		
 	
-	@RequestMapping(value = "/adherent.accession", method = RequestMethod.GET)
+	@RequestMapping(value = "/adherent.accession", method = RequestMethod.GET , produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Adherent> displayOne(@RequestParam("id") long id) {
 		Adherent adherent = adherentDAO.find(id);
 		System.out.println(adherent.toString());
 		
 		return ResponseEntity.status(HttpStatus.OK).body(adherent);
+	}
+	
+	@RequestMapping(value = "/adherent.ajout", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public void CreateAdherent(@RequestBody Adherent adherent) {
+		 adherentDAO.persist(adherent);
+		System.out.println(adherent.toString());
+		
+		//return ResponseEntity.status(HttpStatus.OK).body(adherent);
 	}
 	
 }
--- src/main/java/fr/service/GenericDAO.java
+++ src/main/java/fr/service/GenericDAO.java
@@ -3,7 +3,13 @@ package fr.service;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+
+@ComponentScan
 public class GenericDAO<T> {
 
 	@PersistenceContext 
